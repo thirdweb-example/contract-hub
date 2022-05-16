462bf24f9c51195ec09e02f3aabbb6baa422e62f
Jarrod Watts <jarrodwatts16@gmail.com>
2022-05-16 10:07:48 +1000

    Delete unused stuff for guide

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,20 +4,14 @@ import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 import "../styles/globals.css";
 import Head from "next/head";
 import ThirdwebGuideFooter from "../components/guide/ThirdwebGuideFooter";
-import ThirdwebGuideOverlay from "../components/guide/ThirdwebGuideOverlay";
 import Header from "../components/Header";
 
 // This is the chainId your dApp will work on.
 const activeChainId = ChainId.Mumbai;
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [showGuideOverlay, setShowGuideOverlay] = React.useState(false);
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
-      <ThirdwebGuideOverlay
-        show={showGuideOverlay}
-        setShow={setShowGuideOverlay}
-      />
       <Head>
         <title>thirdweb Contract Example Hub</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
